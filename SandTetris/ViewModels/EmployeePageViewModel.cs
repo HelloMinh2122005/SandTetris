05d8905107c94198fb8e2416fe975832bb4683d4
helloMinh <phandinhminh48@gmail.com>
2024-12-01 22:16:22 +0700

    m

--- SandTetris/ViewModels/EmployeePageViewModel.cs
+++ SandTetris/ViewModels/EmployeePageViewModel.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 
 namespace SandTetris.ViewModels;
 
+
 public partial class EmployeePageViewModel : ObservableObject, IQueryAttributable
 {
     [ObservableProperty]
